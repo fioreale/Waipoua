9034eeb7503584a000524d63004ed3ced14dcaac
fioreale <alefio96@gmail.com>
2020-05-04 18:20:27 +0200

    Javascript for application forms on IntroPages fixed

--- web/Scripts/makeTitle_people.js
+++ web/Scripts/makeTitle_people.js
@@ -2,17 +2,28 @@
 var all_radio = document.getElementById("allRadio3");
 var role_radio = document.getElementById("categoryRadio3");
 var apply_btn = document.getElementById("apply-btn3");
+var menu = document.getElementById("people-menu");
+var content_menu = menu.innerHTML;
+
+document.getElementById("people-menu").remove();
+
 all_radio.onclick = function () {
+    if (document.getElementById("people-menu") != null) {
+        document.getElementById("people-menu").remove();
+    }
     apply_btn.onclick = function () {
-        document.getElementById("title_jumbo3").innerText = "All People"
+        document.getElementById("title_jumbo3").innerText = "All People";
     }
 }
 role_radio.onclick = function () {
+    role_radio.parentElement.parentElement.append(menu);
+    menu.innerHTML = content_menu;
     apply_btn.onclick = function () {
         var index = document.getElementById("select-category3").selectedIndex
         if (index > 0) {
             document.getElementById("title_jumbo3").innerHTML = document.getElementById("select-category3").innerText.split("\n")[index];
             document.getElementById("select-category3").value = document.getElementById("select-category3").innerText.split("\n")[0];
+            document.getElementById("people-menu").remove();
         }
     }
 }
