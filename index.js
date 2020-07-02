0c70d108973f59db2fdd5cdfb8fed0f9df326ed0
fioreale <alefio96@gmail.com>
2020-07-02 11:02:03 +0200

    - very small fix in index.js

--- index.js
+++ index.js
@@ -14,42 +14,44 @@ let {setupDataLayer} = require("./service/DataLayer")
 
 // swaggerRouter configuration
 var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
-  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+    swaggerUi: path.join(__dirname, '/swagger.json'),
+    controllers: path.join(__dirname, './controllers'),
+    useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'public/backend/spec.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(__dirname, 'public/backend/spec.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
 
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
 
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
 
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
 
-  app.use(serveStatic(__dirname + "/public"));
+    app.use(serveStatic(__dirname + "/public"));
+    app.use("/backend", serveStatic(__dirname + "/public/backend", { 'index': ['main.html'] }));
+    app.use("/backend/swaggerui", serveStatic(__dirname + "/public/backend/swaggerui"));
 
-  // Start the server
-  console.log("entering the Data Layer Setup...")
-  setupDataLayer()
-      .then(() => {
-        http.createServer(app).listen(serverPort, function () {
-          console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-          console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    // Start the server
+    console.log("entering the Data Layer Setup...")
+    setupDataLayer()
+        .then(() => {
+            http.createServer(app).listen(serverPort, function () {
+                console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+                console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+            });
+        })
+        .catch(() => {
+            console.log("Creation of the server not succeeded!")
         });
-      })
-      .catch(()=>{
-        console.log("Creation of server not succeeded!")
-      });
 });
